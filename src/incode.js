d6bd30f97f3fb4dceaf50083876a9db35201038e
Alexis Navarro <alexisnavarro23@gmail.com>
2022-11-01 11:13:11 -0600

    1.51.0

--- src/incode.js
+++ src/incode.js
@@ -5,7 +5,6 @@ const clientId = process.env.REACT_APP_CLIENT_ID;
 const incode = create({
   clientId: clientId,
   apiURL: apiURL,
-  lang: "en",
   theme: {
     // main: "",
     // mainButton: {
