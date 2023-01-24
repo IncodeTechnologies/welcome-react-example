eb9c493baa296326d7ed3a7e728191dd25e93016
Alexis Navarro <alexisnavarro23@gmail.com>
2023-01-24 10:07:36 -0600

    translations

--- src/incode.js
+++ src/incode.js
@@ -1,10 +1,23 @@
-import { create } from "@incodetech/welcome";
+// import { create } from "@incodetech/welcome";
 const apiURL = import.meta.env.VITE_USER_SERVER;
 const clientId = import.meta.env.VITE_CLIENT_ID;
 
-const incode = create({
+const incode = OnBoarding.create({
   clientId: clientId,
   apiURL: apiURL,
+  translations: {
+    tutorial: {
+      front1: "Show the front of id",
+      front2: " front ",
+      back1: "show the back of id",
+      back2: " abcd ",
+      selfie1: "take the selfie",
+      selfie2: "remove hats, glasses etc",
+      selfie3: "Give netural expersssions",
+      passport1: "Passport capture1",
+      passport2: "passport",
+    },
+  },
   theme: {
     // main: "",
     // mainButton: {
