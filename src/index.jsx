8ceaaebb56539b202b200e77892b51cf44ad37a7
26diegos <48571714+26diegos@users.noreply.github.com>
2023-07-19 17:40:00 -0600

    Upgrade app to React 18 (#28)

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -17,9 +17,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
